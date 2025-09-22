a2d51ada3992a1bca750293190ac4baef3bfd524
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-09-22 15:26:10 -0500

    added build script

---
+++ build.lua
@@ -0,0 +1,8 @@
+return {
+      name = 'CapyCarts',
+      config = 'nonsteam',
+      version = '0.1',
+      love = '11.5',
+      icon = 'assets/icon.png',
+      platforms = {'windows','linux','mac'},
+}
